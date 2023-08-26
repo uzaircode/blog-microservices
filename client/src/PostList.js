2b6a765cc87fcbcc41f0b9ba10ec0e0572f378d3
Nik Uzair <nikuzairsc@gmail.com>
2023-08-26 12:52:09 +0800

    Displaying Comments

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 function PostList() {
   const [posts, setPosts] = useState({});
@@ -22,8 +23,9 @@ function PostList() {
         key={post.id}
       >
         <div className="card-body">
-          <h3>{[post.title]}</h3>
-          <CommentCreate postId={posts} />
+          <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
